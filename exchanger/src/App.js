a5e1135b5a6fdb2ef1a80d67d174e05af9e1be8f
annagetman <annaporik12@gmail.com>
2020-07-15 20:39:15 +0300

    create new component Header and Nav

--- exchanger/src/App.js
+++ exchanger/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {render} from "react-dom";
+import Header from "./Header/Header";
+import Nav from "./Nav/Nav";
+
 
 class App extends React.Component {
     constructor(props) {
@@ -12,26 +15,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="site">
-                <header>
-                    <div className="top-bar animate-dropdown"></div>
-                    <div className="main-header">
-                        <div className="container">
-                            <h1 className="site-title">React Lite Level</h1>
-                        </div>
-                    </div>
-                    <div className="header-nav">
-                        <div className="container">
-                            <nav>
-                                <ul>
-                                    <li><a href="#">Главная</a></li>
-                                    <li><a href="#">Пункты обмена</a></li>
-                                    <li><a href="#">Контакты</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-
-                </header>
+                <Header />
 
                 <div className="container">
                     <main>
