dfb7388a62d960dbfef8f0fa2568716710e5a11e
annagetman <annaporik12@gmail.com>
2020-07-17 13:51:30 +0300

    add component Nav

--- exchanger/src/Nav/Nav.js
+++ exchanger/src/Nav/Nav.js
@@ -12,7 +12,7 @@ class Nav extends React.Component {
                 <ul>
                     <li><a href="/">Главная</a></li>
                     <li><a href="/">Пункты обмена</a></li>
-                    <li><a href="/">Контакты</a></li>
+                    <li><a href="/about">О нас</a></li>
                 </ul>
             </nav>
         );
