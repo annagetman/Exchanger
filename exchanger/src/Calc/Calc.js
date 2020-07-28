8a0c5c9c9dea33b24fa8298797291eabe38cffda
annagetman <annaporik12@gmail.com>
2020-07-28 14:29:07 +0300

    update css from Calc

--- exchanger/src/Calc/Calc.js
+++ exchanger/src/Calc/Calc.js
@@ -46,7 +46,7 @@ class Calc extends React.Component {
                     <div>
                         <h4>Результат</h4>
                         <ul className="calc-res">
-                            <li>EUR {this.state.result}</li>
+                            <li>EUR {this.state.result.toFixed(2)}</li>
                         </ul>
                     </div>
                 </div>
