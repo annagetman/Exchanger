07977a367281fbbfb677538ab71a8aee50f1236e
annagetman <annaporik12@gmail.com>
2021-02-03 14:14:08 +0300

    on level over

---
+++ src/About/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './About.css'
+
+
+class About extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //
+    // }
+
+    render() {
+        return (
+           <div>
+               <div className="game"></div>
+           </div>
+        );
+    }
+}
+
+export default About;
---
+++ src/Points/Points.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Points.css'
+
+
+class Points extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        return (
+           <div>
+               direct someone's attention toward something by extending
+               one's finger or something held in one's hand.
+           </div>
+
+        );
+    }
+}
+
+export default Points;
